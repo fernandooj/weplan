a50020f6289529322694dca270d1cde41856f435
Sir Ortiz <sirortiz@fermac.local>
2018-08-21 01:59:37 -0500

    remove animation app

--- weplan/App.js
+++ weplan/App.js
@@ -43,52 +43,7 @@ import detallePlanPublicoComponent from './src/planesPublicos/detallePlanPublico
 export const URL = 'https://appweplan.com/';
 axios.defaults.baseURL = URL;
 
-const transitionConfig = () => {
-return {
-  transitionSpec: {
-    duration: 750,
-    easing: Easing.out(Easing.poly(4)),
-    timing: Animated.timing,
-    useNativeDriver: true,
-  },
-  screenInterpolator: sceneProps => {
-      const { position, layout, scene, index, scenes } = sceneProps
-      const toIndex = index
-      const thisSceneIndex = scene.index
-      const height = layout.initHeight
-      const width = layout.initWidth
-
-      const translateX = position.interpolate({
-        inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
-        outputRange: [width, 0, 0]
-      })
-
-      // Since we want the card to take the same amount of time
-      // to animate downwards no matter if it's 3rd on the stack
-      // or 53rd, we interpolate over the entire range from 0 - thisSceneIndex
-      const translateY = position.interpolate({
-        inputRange: [0, thisSceneIndex],
-        outputRange: [height, 0]
-      })
-
-      const slideFromRight = { transform: [{ translateX }] }
-      const slideFromBottom = { transform: [{ translateY }] }
-
-      const lastSceneIndex = scenes[scenes.length - 1].index
-
-      // Test whether we're skipping back more than one screen
-      if (lastSceneIndex - toIndex > 1) {
-        // Do not transoform the screen being navigated to
-        if (scene.index === toIndex) return
-        // Hide all screens in between
-        if (scene.index !== lastSceneIndex) return { opacity: 0 }
-        // Slide top screen down
-        return slideFromBottom
-      } 
-
-      return slideFromRight
-    },
-}}
+ 
 
 
 
@@ -119,7 +74,7 @@ const NavigationApp = StackNavigator({
     insertCode2:   {screen: insertCodeComponent2 },
     nuevoPassword: {screen: nuevoPasswordComponent },
     infoPlan:      {screen: infoPlanComponent },
-},{ headerMode: 'none', transitionConfig })
+},{ headerMode: 'none' })
  
 const NavigationAppLogin = StackNavigator({  
     Home:          {screen: homeComponent}, 
@@ -152,7 +107,7 @@ const NavigationAppLogin = StackNavigator({
     planesPublicos:{screen: planesPublicosComponent },
     facturacion:   {screen: facturacionComponent },
     detallePlanPublico:{screen: detallePlanPublicoComponent },
-},{ headerMode: 'none', transitionConfig})
+},{ headerMode: 'none'})
 
 
  
